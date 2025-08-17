386665fac015b5c089150f4d344b6ddcaecc28cb
Yasin <yasinkara99@hotmail.com>
2025-08-17 19:52:21 +0300

    created breakpoints for responsive design and updated mixin use at settings-menu.vue

--- src/components/settings-menu.vue
+++ src/components/settings-menu.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="settings-menu">
-    <button id="settingsIcon" @click="toggleMenu">
-      <svg id="settings1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" class="settings-svg"
+    <button ref="menu"  id="settingsIcon" @click="toggleMenu">
+      <svg id="settings1" ref="a"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" class="settings-svg"
         aria-label="Ayarlar" :class="{ rotate1: visibility }">
         <path
           d="M259.1 73.5C262.1 58.7 275.2 48 290.4 48L350.2 48C365.4 48 378.5 58.7 381.5 73.5L396 143.5C410.1 149.5 423.3 157.2 435.3 166.3L503.1 143.8C517.5 139 533.3 145 540.9 158.2L570.8 210C578.4 223.2 575.7 239.8 564.3 249.9L511 297.3C511.9 304.7 512.3 312.3 512.3 320C512.3 327.7 511.8 335.3 511 342.7L564.4 390.2C575.8 400.3 578.4 417 570.9 430.1L541 481.9C533.4 495 517.6 501.1 503.2 496.3L435.4 473.8C423.3 482.9 410.1 490.5 396.1 496.6L381.7 566.5C378.6 581.4 365.5 592 350.4 592L290.6 592C275.4 592 262.3 581.3 259.3 566.5L244.9 496.6C230.8 490.6 217.7 482.9 205.6 473.8L137.5 496.3C123.1 501.1 107.3 495.1 99.7 481.9L69.8 430.1C62.2 416.9 64.9 400.3 76.3 390.2L129.7 342.7C128.8 335.3 128.4 327.7 128.4 320C128.4 312.3 128.9 304.7 129.7 297.3L76.3 249.8C64.9 239.7 62.3 223 69.8 209.9L99.7 158.1C107.3 144.9 123.1 138.9 137.5 143.7L205.3 166.2C217.4 157.1 230.6 149.5 244.6 143.4L259.1 73.5zM320.3 400C364.5 399.8 400.2 363.9 400 319.7C399.8 275.5 363.9 239.8 319.7 240C275.5 240.2 239.8 276.1 240 320.3C240.2 364.5 276.1 400.2 320.3 400z"
@@ -25,7 +25,6 @@
         <RouterLink to='/about' aria-label="Uygulama Hakkında sayfasına git">
           <li>Uygulama Hakkında</li>
         </RouterLink>
-
       </ul>
     </nav>
   </div>
@@ -42,8 +41,8 @@ export default {
     toggleMenu() {
       this.visibility = !this.visibility
     },
-    closeMenu() {
-      console.log(this.$refs)
+    closeMenu(event) {
+      console.log(event.target, event.currentTarget);
     }
   },
   mounted() {
@@ -55,7 +54,8 @@ export default {
 }
 </script>
 <style lang="sass" scoped>
-@use '@/assets/style/_variables.sass' as v
+@use '@/assets/style/_variables.sass' as *
+@use '@/assets/style/breakpoints.sass' as *
 
 .visibility
   display: none
@@ -79,7 +79,7 @@ export default {
   *
     position: absolute
     transition-duration: .5s
-    color: v.$menuIconBg
+    color: $menuIconBg
     width: 15px
 
   #settings1
@@ -90,6 +90,17 @@ export default {
     right: 0
     bottom: 0
 
+  &:hover
+    cursor: pointer
+
+  //responsive
+  +changeStyle($tablet)
+    background: red
+
+  +changeStyle($laptop)
+    background: blue
+
+    
 .rotate1
   rotate: -150deg
 
@@ -97,10 +108,10 @@ export default {
   rotate: 150deg
 
 nav
-  border: v.$menuBorder
-  background: v.$menuBg
-  border-radius: v.$menuBorderR
-  padding: v.$menuPadding
+  border: $menuBorder
+  background: $menuBg
+  border-radius: $menuBorderR
+  padding: $menuPadding
   max-width: 50%
   min-width: 25%
 
@@ -111,7 +122,7 @@ nav
     gap: 4px
 
   li
-    //background: v.$menuLinkBg
+    //background: $menuLinkBg
     display: flex
     padding: 4px 3px
     justify-content: center
@@ -120,15 +131,15 @@ nav
     transition: .1s
     border-radius: 5px
     &:hover
-      background: v.$menuLiHover
+      background: $menuLiHover
       scale: 1.06
     &:active
       scale: 1
   a
-    color: v.$menuText
+    color: $menuText
     text-decoration: none
-    font-family: v.$menuFont
-    font-size: v.$menuFontSize
+    font-family: $menuFont
+    font-size: $menuFontSize
     font-weight: 500
 
 </style>
