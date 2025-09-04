37028ab3dcfd88efdca3f6bb72dc15bb22873e91
Yasin <yasinkara99@hotmail.com>
2025-09-04 12:12:39 +0300

    Feat(ui): add shadow to anno
    Why:
    -Increase it's appearance

--- src/components/announcement.vue
+++ src/components/announcement.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="announcement">
- <div id="busId"><span>{{ busId }}</span></div>
- <div id="busAnno">
-   {{ busAnno }}
- </div>
+    <div id="busId"><span>{{ busId }}</span></div>
+    <div id="busAnno">
+      {{ busAnno }}
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: 'announcement',
   data() {
- return {
-   busId: '89km',
-   busAnno: "'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into ele"
- }
+    return {
+      busId: '89km',
+      busAnno: "'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into ele"
+    }
   }
 }
 </script>
@@ -25,6 +25,11 @@ export default {
   align-items: center
   gap: 35px
   padding: 29px 13px
+  background-color: #1e2a38
+  border-radius: 12px
+  padding: 20px
+  margin: 20px 0
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.4)
 
 #busId
  min-width: 85px
