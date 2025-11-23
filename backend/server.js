461c26648ffeed7aed261ada1f7c633960709439
Yasin KARA <yasinkara99@hotmail.com>
2025-11-24 00:12:20 +0300

    deleted unused comment line

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 const corsOptions = {
-  origin: 'http://localhost:5173', // Vite varsayılan portu
+  origin: 'http://localhost:5173',
   optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
